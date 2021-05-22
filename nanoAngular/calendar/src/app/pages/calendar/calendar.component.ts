67b6906b550ae47df014eb595215cb23b8a8002a
miguelesco <miuelangel1999@gmail.com>
2021-05-22 14:52:49 -0400

    'icons'

--- nanoAngular/calendar/src/app/pages/calendar/calendar.component.ts
+++ nanoAngular/calendar/src/app/pages/calendar/calendar.component.ts
@@ -17,5 +17,8 @@ export class CalendarComponent implements OnInit {
   testArray = [
     1,2,3,4,5,6,7,8,9,10,11,12,13,14
   ]
+  previusMonth() {
+    console.log('atras')
+  }
 
 }
--- nanoAngular/calendar/src/app/pages/calendar/calendar.module.ts
+++ nanoAngular/calendar/src/app/pages/calendar/calendar.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+
 import { CalendarComponent } from './calendar.component';
 import { CalendarRoutingModule } from './calendar.routing';
 
@@ -12,7 +16,9 @@ import { CalendarRoutingModule } from './calendar.routing';
   imports: [
     CommonModule,
     MatGridListModule,
-    CalendarRoutingModule
+    MatIconModule,
+    MatButtonModule,
+    CalendarRoutingModule,
   ],
   exports: [
     CalendarComponent
